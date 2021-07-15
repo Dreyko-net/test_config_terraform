9435d8ea9fc40ce2394cb1548fc00dcd4c464a85
Evgeny <Dreyko-net@yandex.com>
2021-07-15 10:59:13 +0000

    Исправил все ошибки и протестировал

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ health_check {
   healthy_threshold = 2
   unhealthy_threshold =2
   timeout = 3
-  target = "HTTP:80/"
+  target = "HTTP:80/health-check-web-server.html"
   interval = 10
 }
 tags = {
--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+cat << EOF > /var/www/html/health-check-web-server.html
+<html>
+<body>
+
+</body>
+</html>
+EOF
 
 sudo service httpd start
 chkconfig httpd on
